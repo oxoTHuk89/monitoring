dad9d6345d67103b830ec4f8c8c2e5e6111e36ed
oxoTHuk89 <burikovs@gmail.com>
2016-09-18 22:11:41 +0300

    Remove trash

--- index.php
+++ index.php
@@ -16,27 +16,26 @@ $getServers = $getServers->getServers($dbh, $DataBase);
 
 $getGames = new ServerInfo();
 $getGames = $getGames->getGames($dbh, $DataBase);
-if(ADMIN){
-	if(isset($_POST['add_srv'])){
-		$setServers = new ServerInfo();
-		$setServers = $setServers->setServers($dbh, $_POST, $DataBase);
-		echo $setServers;
-			exit();
-	}
-	if(isset($_POST['serverdel'])){
-		$deleteServers = new ServerInfo();
-		$deleteServers = $deleteServers->deleteServers($dbh, $_POST);
-		echo $deleteServers;
-			exit();
-	}
-
+if (ADMIN) {
+    if (isset($_POST['add_srv'])) {
+        $setServers = new ServerInfo();
+        $setServers = $setServers->setServers($dbh, $_POST, $DataBase);
+        echo $setServers;
+        exit();
+    }
+    if (isset($_POST['serverdel'])) {
+        $deleteServers = new ServerInfo();
+        $deleteServers = $deleteServers->deleteServers($dbh, $_POST);
+        echo $deleteServers;
+        exit();
+    }
 }
 
 $smarty = new Smarty();
 
 $chmod = (int)substr(sprintf('%o', fileperms($smarty->getCompileDir())), -4);
-if($chmod != 777){
-	die('Нет парв на запись в папку '.$smarty->getCompileDir());
+if ($chmod != 777) {
+    die('Нет парв на запись в папку ' . $smarty->getCompileDir());
 }
 $smarty->assign('getServers', $getServers);
 $smarty->assign('getGames', $getGames);
